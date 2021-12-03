3966b13fffb083e0ef4c66969b7d7fb901826c4b
elliaschabloz <67370331+elliaschabloz@users.noreply.github.com>
2021-12-03 18:18:41 +0100

    Update ConnexionGUI.java

--- chatting_project/src/view/ConnexionGUI.java
+++ chatting_project/src/view/ConnexionGUI.java
@@ -1,18 +1,45 @@
 package view;
+import javax.swing.*; 
 import model.*;
+import java.awt.*;  
+import java.awt.event.*;  
 
-
-public class ConnecionGUI {
-	    public String JFrame;
-	    public ListUser listUser;
-
-	    public void Connect(String pseudo) {
-	    }
-	    private boolean CheckPseudoUnicity(String pseudo) {
-	    	boolean check= false;
-	    	
-	    	return check;
-	    }
-
+public class ConnexionGUI {
+	public ListUser listUser;
+	
+	public static void main(String[] args) {  
+		 JFrame f=new JFrame("CONNEXION");  
+		 JTextField tf=new JTextField();
+		 final JLabel l = new JLabel();
+		 tf.setBounds(50,50, 150,20); 
+		 l.setBounds(50,100, 150,20);  
+		 JButton b=new JButton("Connect");  
+		 b.setBounds(50,150,100,30);  
+		 
+		 b.addActionListener(new ActionListener() {
+			 public void actionPerformed(ActionEvent e) {  
+			        try{ 
+			        //Connect("ok");
+			        String reussite="Your are connected";  
+			        l.setText(reussite);
+			        }catch(Exception ex){System.out.println(ex);}  
+			    } 
+		 });
+		 
+		 f.add(b);f.add(tf);f.add(l);
+		 f.setSize(400,400);  
+		 f.setLayout(null);  
+		 f.setVisible(true); 
+		 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
+
+	
+	public static void Connect(String pseudo) {
+	}
+	private boolean CheckPseudoUnicity(String pseudo) {
+		boolean check= false;
+	    return check;
+	}
+	 
+}
 
