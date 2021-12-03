dc7eda80427c7951403080e31b9f4dc4a55d213d
djihadi ahamdy <djihadi.ahamdy@gmail.com>
2021-12-03 15:59:21 +0100

    Classe Clock, Connexion GUI and ListUser implement base

--- chatting_project/src/model/ListUser.java
+++ chatting_project/src/model/ListUser.java
@@ -1,5 +1,26 @@
 package model;
+//import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
 
 public class ListUser {
+
+	public userList ConnectedUsers;
+
+  public boolean isConnected(String pseudo) {
+  	boolean check = false;
+  	
+  	return check;
+  }
+
+
+	//  On doit créer un thread qui sera en écoute permanenet pour mettre à jour 
+	//  la list des utilisateurs 
+	  private void updateListUser() {
+	  }
+	  
+	  public static void main(String args[]) {
+	  	
+	  	
+	  }
 
 }
--- chatting_project/src/view/Connexion_GUI.java
+++ chatting_project/src/view/Connexion_GUI.java
@@ -1,5 +1,18 @@
 package view;
+import model.*;
 
-public class Connexion_GUI {
 
-}
+public class ConnecionGUI {
+	    public String JFrame;
+	    public ListUser listUser;
+
+	    public void Connect(String pseudo) {
+	    }
+	    private boolean CheckPseudoUnicity(String pseudo) {
+	    	boolean check= false;
+	    	
+	    	return check;
+	    }
+
+	}
+
