0e30fc84bac430a105ebf8162847a4acca38c9b5
djihadi ahamdy <djihadi.ahamdy@gmail.com>
2021-12-06 16:05:14 +0100

    Merge branch 'master' of https://github.com/elliaschabloz/chatting_project_code.git

--- chatting_project/src/model/ListUser.java
+++ chatting_project/src/model/ListUser.java
@@ -1,8 +1,12 @@
 package model;
 //import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
-public class ListUser {
+public class ListUser implments Runnable {
 
 	public userList ConnectedUsers;
 
@@ -12,13 +16,48 @@ public class ListUser {
   	return check;
   }
 
-
 	//  On doit créer un thread qui sera en écoute permanenet pour mettre à jour 
 	//  la list des utilisateurs 
-	  private void updateListUser() {
+  private void updateListUser() {
+		private DatagramSocket socket;
+		private boolean running;
+		private byte[] buf = new byte[256];
+		  
+		socket = new DatagramSocket(2020);
+		
+		running = true;
+		while (running) {
+			DatagramPacket packet = new DatagramPacket(buf, buf.length);
+	        try {
+				socket.receive(packet);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		    
+		    InetAddress address = packet.getAddress();
+		    int port = packet.getPort();
+		    packet = new DatagramPacket(buf, buf.length, address, port);
+		    String received  = new String(packet.getData(), 0, packet.getLength());
+		    
+		    if (received.equals("end")) {
+		        running = false;
+		        continue;
+		    }
+		    try {      	
+		    	//renvoie le paquet reçue 
+				socket.send(packet);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		socket.close();
+	}
+  public void run() {
+	  updateListUser();
+  }
 		  
 		  
-	  }
-	  
-
 }
+	  
