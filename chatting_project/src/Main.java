3c482c142e14bedf2ed60250f34137c680c8f384
Chabloz Ellias <chabloz@insa-20069>
2022-01-06 12:10:02 +0100

    tcp

--- chatting_project/src/Main.java
+++ chatting_project/src/Main.java
@@ -1,6 +1,7 @@
 package src;
 
 import java.io.IOException;
+import java.net.Socket;
 
 import src.control.*;
 import src.model.*;
@@ -26,9 +27,30 @@ public class Main {
 				TCP.Server(1234);
 			}
 		};
-		runSrv.run();
+		Thread th = new Thread(runSrv);
+		th.start();
 		
 		String hostname = "insa-20072";
-		TCP.Client(hostname, 1234);
+		/*
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+		//TCP.Client(hostname, 1234);
+		Socket clientSocket = TCP.StartChattingSessionWith(hostname, 1234);
+		TCP.SendTo(clientSocket, "coucou");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		TCP.SendTo(clientSocket, "c'est moi");
+		System.out.println("après c'est moi");
+
+		TCP.CloseChattingSessionWith(clientSocket);
 	}
 }
